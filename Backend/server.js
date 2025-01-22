7e1dd712609e297db6daf0c30916900d3ca9734f
ninja-NikhilChauhan <87414959+content-cn@users.noreply.github.com>
2025-01-22 16:06:43 +0530

    Week-3-class-1 files

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-// const userRoutes = require('./routes/userRoutes'); // Import user routes
+
 
 const authRoutes = require('./Routes/authroutes');
+const formRoute = require('./Routes/registrationformRoute')
 
 
 const app = express();
@@ -28,6 +29,8 @@ app.get('/',(req, res)=>{
 })
 
 app.use('/api/auth',authRoutes)
+app.use('/api/form',formRoute)
+
 // Define the port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
